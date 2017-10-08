e64d6e98cd175f87764256b1730c18c57e34a20a
Brock Ellefson <brockilli03@gmail.com>
2017-10-08 17:45:00 -0600

    Framework

---
+++ Xanterra2d/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+
+    public Transform target;
+    Camera myCamera;
+	// Use this for initialization
+	void Start () {
+        myCamera = GetComponent<Camera>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        myCamera.orthographicSize = (Screen.height / 100f) / 2f;
+
+        if (target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, 0.1f) + new Vector3(0,0, -10);
+        }
+    }
+}
---
+++ Xanterra2d/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    private Rigidbody2D rbody;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        rbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (movementVector != Vector2.zero)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("inputX", movementVector.x);
+            anim.SetFloat("inputY", movementVector.y);
+        }
+        else{
+            anim.SetBool("isWalking", false);
+        }
+
+        rbody.MovePosition(rbody.position + movementVector * Time.deltaTime);
+	}
+}
